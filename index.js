d2f5067a33552696a56f6c1a2272570824be2cd7
andrew-yip <andrew.yip22@gmail.com>
2022-12-25 17:48:27 -1000

    added request dependency

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
-const request = require('request-promise')
+const request = require('request-promise');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 9000;
 
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Amazon Scraper API');
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
